4935eeec0b294ec22593553efbf84e5c520e8b90
trudova <liubovtrudova@gmail.com>
2022-03-21 15:27:53 -0400

    initial commit

---
+++ src/test/java/com/liv/OrmonetomanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrmonetomanyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
